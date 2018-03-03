eda853dc36818bbdb8659a3de501ce5ca3e1b6b6
Norm Cho <normancho@gmail.com>
2018-03-03 16:44:24 -0600

    plot2code.R

---
+++ plot2code.R
@@ -0,0 +1,16 @@
+## reading the data and classification code guides
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## loading the necessary packages
+> library(dplyr)
+> library(ggplot2)
+
+## dataset of Balitmore, named after its football team
+ravens <- subset(NEI, NEI$fips == "24510")
+
+## summing the emissions by year
+ravens_sumpm25 <-with(ravens, aggregate(ravens$Emissions, by = list(year), sum))
+
+## graph using plot from the base plotting system
+plot(ravens_sumpm25, type="o", lwd=3, main="Total Baltimore City PM25 Emissions from 1999, 2002, 2005, 2008",   xlab = "Year", ylab = "Tons", pch=10)
