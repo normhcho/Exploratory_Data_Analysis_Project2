5fe2b25aa186fbc74adc597b123b7601b4d4bd87
Norm Cho <normancho@gmail.com>
2018-03-03 20:17:29 -0600

    plotcode4.R

---
+++ plotcode4.R
@@ -0,0 +1,19 @@
+## reading the data and classification code guides
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## loading the necessary packages
+library(dplyr)
+library(ggplot2)
+
+## merging the NEI and SCC files by SCC
+NEISCC <- merge(NEI,SCC, by="SCC")
+
+## selecting the coal records based on the string "coal" in the Short.Name category
+coal <- NEISCC[grepl("Coal",NEISCC$Short.Name,ignore.case=TRUE),]
+
+## summming the emissions by coal records
+coalsum <- aggregate(Emissions ~ year, coal, sum)
+
+## line graph using ggplot from the ggplot2 plotting system
+ggplot(coalsum, aes(year, Emissions))+ geom_line() + xlab("Year") + ylab("Tons") + ggtitle("Total Emissions by Coal Sources in 1999, 2002, 2005, 2008")
