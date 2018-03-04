340acb68a18e0f541ce70ae0d71ce8d5b6b665bf
Norm Cho <normancho@gmail.com>
2018-03-03 20:56:20 -0600

    Update plot3code.R

--- plot3code.R
+++ plot3code.R
@@ -2,10 +2,8 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## loading the necessary packages
-> library(dplyr)
-> library(ggplot2)
-
+## loading the necessary package
+library(ggplot2)
 
 ## dataset of Balitmore, named after its football team
 ravens <- subset(NEI, NEI$fips == "24510")
@@ -14,4 +12,8 @@ ravens <- subset(NEI, NEI$fips == "24510")
 ravens_type <- aggregate(Emissions ~ year + type, ravens, sum)
 
 ## graph using ggplot from the ggplot2 plotting system
-ggplot(ravens_type, aes(year, Emissions, color = type)) + geom_line() + xlab("Year") + ylab("Tons") + ggtitle("Total Emissions by Type in Baltimore in 1999, 2002, 2005, 2008")
+ggplot(ravens_type, aes(year, Emissions, color = type)) +
+  geom_line() +
+  xlab("Year") +
+  ylab("Tons") +
+  ggtitle("Total Emissions by Type in Baltimore in 1999, 2002, 2005, 2008")
